f2210412effbf4d483884dbe3af863a0923b3e6a
Tryell Wellick <omshingare120@gmail.com>
2024-07-02 18:28:16 +0530

    UPDATE

--- index.php
+++ index.php
@@ -32,10 +32,9 @@
         </style>
     </head>
     <body id="page-top">
-        <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
+                <a class="navbar-brand" href="#page-top">TuneTuna</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
@@ -43,7 +42,8 @@
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#dwnmusic">Download Music</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#devs">developers</a></li>
                         <li class="nav-item"><a class="nav-link" href="#signup">Contact</a></li>
                     </ul>
                 </div>
@@ -68,39 +68,58 @@
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-8">
-                        <h2 class="text-white mb-4">Built with Bootstrap 5</h2>
+                        <h2 class="text-white mb-4">Vibe with us,❤️ </h2>
                         <p class="text-white-50">
-                            Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
-                            <a href="https://startbootstrap.com/theme/grayscale/">the preview page.</a>
-                            The theme is open source, and you can use it for any purpose, personal or commercial.
+                        TuneTuna is a powerful music downloader that leverages the Savan API, providing a seamless and efficient experience for downloading music. Built with PHP, MySQL, HTML, and Bootstrap Material Theme, TuneTuna offers a modern and user-friendly interface.
+                            <a href="https://github.com/ShingareOm/TuneTuna/" target="_top">click here</a>
                         </p>
                     </div>
                 </div>
-                <img class="img-fluid" src="./assets/img/ipad.png" alt="..." />
+                <img class="img-fluid" src="./assets/img/tabla.png" alt="..." />
             </div>
         </section>
-        <!-- Projects-->
-        <section class="projects-section bg-light" id="projects">
+
+
+
+
+
+        <section class="projects-section bg-light" id="dwnmusic">
             <div class="container px-4 px-lg-5">
-                <!-- Featured Project Row-->
-                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
-                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="./assets/img/bg-masthead.jpg" alt="..." /></div>
-                    <div class="col-xl-4 col-lg-5">
-                        <div class="featured-text text-center text-lg-left">
-                            <h4>Shoreline</h4>
-                            <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
-                        </div>
-                    </div>
-                </div>
-                <!-- Project One Row-->
                 <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
-                    <div class="col-lg-6"><img class="img-fluid" src="./assets/img/demo-image-01.jpg" alt="..." /></div>
+                    <!-- <div class="col-lg-6"><img class="img-fluid" src="./assets/img/omshingare.gif" alt="..." /></div> -->
                     <div class="col-lg-6">
                         <div class="bg-black text-center h-100 project">
                             <div class="d-flex h-100">
                                 <div class="project-text w-100 my-auto text-center text-lg-left">
-                                    <h4 class="text-white">Misty</h4>
-                                    <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                                    <h4 class="text-white">Music Search</h4>
+                                    <p class="mb-0 text-white-50">Code is being to add</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+
+
+
+
+        <!-- Projects-->
+        <section class="projects-section bg-light" id="devs">
+            <div class="container px-4 px-lg-5">
+                <!-- Featured Project Row-->
+                <!-- Project One Row-->
+                <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="./assets/img/omshingare.gif" alt="..." /></div>
+                    <div class="col-lg-6">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 class="text-white">Om Shingare</h4>
+                                    <p class="mb-0 text-white-50">I'm a learner who love's solving complex problems.</p>
                                 </div>
                             </div>
                         </div>
@@ -108,13 +127,13 @@
                 </div>
                 <!-- Project Two Row-->
                 <div class="row gx-0 justify-content-center">
-                    <div class="col-lg-6"><img class="img-fluid" src="./assets/img/demo-image-02.jpg" alt="..." /></div>
+                    <div class="col-lg-6"><img class="img-fluid" src="./assets/img/rushishingare.gif" alt="..." /></div>
                     <div class="col-lg-6 order-lg-first">
                         <div class="bg-black text-center h-100 project">
                             <div class="d-flex h-100">
                                 <div class="project-text w-100 my-auto text-center text-lg-right">
-                                    <h4 class="text-white">Mountains</h4>
-                                    <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well!</p>
+                                    <h4 class="text-white">Rushikesh Shingare</h4>
+                                    <p class="mb-0 text-white-50">I'm a learner who love's solving complex problems.</p>
                                 </div>
                             </div>
                         </div>
@@ -176,7 +195,7 @@
                                 <i class="fas fa-map-marked-alt text-primary mb-2"></i>
                                 <h4 class="text-uppercase m-0">Address</h4>
                                 <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50">4923 Market Street, Orlando FL</div>
+                                <div class="small text-black-50">MAHARASHTRA, INDIA ❤️</div>
                             </div>
                         </div>
                     </div>
@@ -186,7 +205,7 @@
                                 <i class="fas fa-envelope text-primary mb-2"></i>
                                 <h4 class="text-uppercase m-0">Email</h4>
                                 <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50"><a href="#!">hello@yourdomain.com</a></div>
+                                <div class="small text-black-50"><a href="#!"><a href="mailto:contactus@tunetuna.me">contactus@tunetuna.me</a></a></div>
                             </div>
                         </div>
                     </div>
@@ -196,7 +215,8 @@
                                 <i class="fas fa-mobile-alt text-primary mb-2"></i>
                                 <h4 class="text-uppercase m-0">Phone</h4>
                                 <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50">+1 (555) 902-8832</div>
+                                <div class="small text-black-50"><a href="tel:+918767561500">+918767561500</a></div>
+                                <div class="small text-black-50"><a href="tel:+918080084167">+918080084167</a></div>
                             </div>
                         </div>
                     </div>
